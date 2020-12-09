72429d6efbcf4437d79e171e383ae37160121183
Sumit <beniwal.sumit.0055@gmail.com>
2020-12-09 20:14:57 +0530

    first commit

---
+++ exam_page/src/App.js
@@ -0,0 +1,51 @@
+import { Component } from 'react';
+import './App.css';
+import ExamPage from './components/ExamPage/ExamPage';
+import StartPage from './containers/StartPage/StartPage';
+import axios from 'axios';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      examStarted: false
+    };
+  }
+
+  componentWillMount() {
+    this.fetchData();
+  }
+
+  fetchData() {
+          var newState = null;
+          axios.get("http://5.181.217.46/DesignFacility/useGETMethodForTheResponse/Sumit_Kumar")
+              .then(response => {
+                  console.log(response.data);
+                  newState = response.data.exam;
+                  this.setState({ exam: newState });
+              });
+          console.log(newState);
+      }
+
+  onStart = () => {
+    if(this.state.exam){
+      this.setState({
+        examStarted: true
+      });
+    }
+  }
+
+  render() {
+    var exam = <StartPage onStart={this.onStart}></StartPage>;
+    if (this.state.examStarted) {
+      exam = <ExamPage exam={this.state.exam} />
+    }
+    return (
+      <div className="App">
+        {exam}
+      </div>
+    );
+  }
+}
+
+export default App;
